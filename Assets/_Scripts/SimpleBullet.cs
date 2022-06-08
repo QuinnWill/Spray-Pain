882ab1e44405f03cb24b20a1b783c61674569bfa
tlw66 <tlw66@drexel.edu>
2022-06-07 22:44:24 -0400

    idk some shit

--- Assets/_Scripts/SimpleBullet.cs
+++ Assets/_Scripts/SimpleBullet.cs
@@ -18,18 +18,33 @@ public class SimpleBullet : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        var player = collision.collider.CompareTag("Player");
+        var wall = collision.collider.CompareTag("Wall");
         var enemy = collision.gameObject.GetComponent<HealthSystem>();
 
-        if (enemy)
+        if (enemy & player)
         {
             enemy.AddHealth(-10);
         }
-        Destroy(gameObject);
+
+        if(player | wall)
+            Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 3)
+        var player = collision.gameObject.CompareTag("Player");
+        var wall = collision.gameObject.CompareTag("Wall");
+        var enemy = collision.gameObject.GetComponent<HealthSystem>();
+
+        if (enemy & player)
+        {
+            enemy.AddHealth(-10);
+        }
+
+        if (player | wall)
+            Destroy(gameObject);
+        /*if (collision.gameObject.layer == 3)
         {
             var enemy = collision.GetComponent<HealthSystem>();
 
@@ -38,7 +53,7 @@ public class SimpleBullet : MonoBehaviour
                 enemy.AddHealth(-10);
             }
             Destroy(gameObject);
-        }
+        }*/
 
     }
 }
